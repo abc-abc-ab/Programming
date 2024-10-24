821987bcf1fb02bd06e8293bd52ebcd07c4fd0f0
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-10-24 15:00:00 +0900

    `getContent("2d")` to `getContext("2d")`

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
   try{
     const p = d.getElementById("paragraph"),
     cnv = d.querySelector("canvas"),
-    ctx = cnv.getContent("2d");
+    ctx = cnv.getContext("2d");
   }
   catch(e){
     alert(e);
