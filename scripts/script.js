0a80c12cf139eb751721d8b8fae2d1b74fa2e2d1
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-10-25 00:09:32 +0900

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@
     const p = d.getElementById("paragraph"),
     cnv = d.querySelector("canvas"),
     ctx = cnv.getContext("2d");
+    ctx.strokeRect(0, 0, 100, 50);
   }
   catch(e){
     alert(e);
