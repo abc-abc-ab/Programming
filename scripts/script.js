514be7107462c6faa505cef47b73a69cba8ed4e0
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-10-26 14:50:47 +0900

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let x, y, dx, dy;
+let x, y, dx = 0;
 ((d, t)=>{
   try{
     const rad2deg = rad => (rad / Math.PI) * 180,
@@ -16,12 +16,25 @@ let x, y, dx, dy;
       ctx.fill();
     ctx.closePath();
     t.setInterval(()=>{
+      ctx.clearRect(0, 0, 3*x, 3*y)
       ctx.beginPath();
       ctx.arc(x, y, 10, 0, deg2rad(360));
       ctx.fillStyle = "#10a0ff";
       ctx.fill();
       ctx.closePath();
+      x += dx;
     }, 10)
+    t.addEventListener("keydown", (e)=>{
+      if (e.key === "ArrowLeft"){
+        dx = -10;
+      }
+      else if(e.key === "ArrowRight"){
+        dx = 10;
+      }
+      else{
+        dx = 0;
+      }
+    })
   }
   catch(e){
     alert(e);
