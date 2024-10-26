6ace5a7ea12d48feced0a71a29c5f7aae23600e8
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-10-26 14:18:21 +0900

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
+let x, y, dx, dy;
 ((d, t)=>{
   try{
-    const p = d.getElementById("paragraph"),
+    const rad2deg = rad => (rad / Math.PI) * 180,
+    deg2rad = deg => (rad / 180) * Math.PI,
+    p = d.getElementById("paragraph"),
     cnv = d.querySelector("canvas"),
-    ctx = cnv.getContext("2d");
-    ctx.strokeRect(0, 0, 300, 150);
+    ctx = cnv.getContext("2d"),
+    width = cnv.style.width*(3/4),
+    height = cnv.style.height*(3/4);
+    ctx.strokeRect(0, 0, width, height);
+    ctx.beginPath();
+      ctx.arc();
   }
   catch(e){
     alert(e);
