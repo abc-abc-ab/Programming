60890f08497499d84beb8bcfbdea05ea35a9457f
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-10-26 14:32:22 +0900

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,12 +8,20 @@ let x, y, dx, dy;
     ctx = cnv.getContext("2d"),
     width = cnv.width,
     height = cnv.height;
+    [x, y] = [width/2, height/2];
     ctx.strokeRect(0, 0, width, height);
     ctx.beginPath();
-      ctx.arc(width/2, height/2, 10, 0, deg2rad(360));
+      ctx.arc(x, y, 10, 0, deg2rad(360));
       ctx.fillStyle = "#10a0ff";
       ctx.fill();
     ctx.closePath();
+    t.setInterval(()=>{
+      ctx.beginPath();
+      ctx.arc(x, y, 10, 0, deg2rad(360));
+      ctx.fillStyle = "#10a0ff";
+      ctx.fill();
+      ctx.closePath();
+    }, 10)
   }
   catch(e){
     alert(e);
