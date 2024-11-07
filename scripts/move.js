239084d1f3ed51d7e1668ac260894d2bb9ff4706
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-11-08 08:34:09 +0900

    Update

--- scripts/move.js
+++ scripts/move.js
@@ -1,3 +1,9 @@
 ((d, t)=>{
-    const div = d.querySelector("div");
+    d.head.insertAdjacentHTML("beforeend", `<style></style>`);
+    const div = d.querySelector("div"),
+    style = d.querySelector("style");
+
+    style.textContent = `div{
+    transform: translate(45deg)
+}`;
 })(document, window)
