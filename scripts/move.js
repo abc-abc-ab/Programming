ed32994cf5abc12d0325d333d231eb428842fa0b
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-11-14 16:53:31 +0900

    Update

--- scripts/move.js
+++ scripts/move.js
@@ -1,9 +1,9 @@
 ((d, t)=>{
     d.head.insertAdjacentHTML("beforeend", `<style></style>`);
-    const div = d.querySelector("div"),
+    const div = d.querySelector("div")/*,
     style = d.querySelector("style");
 
     style.textContent = `div{
-    transform: rotate(45deg)
-}`;
+    transform: rotate(45deg);
+}`*/;
 })(document, window)
